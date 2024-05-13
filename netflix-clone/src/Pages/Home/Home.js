39ff79cafc5e10322fb0f510988bc5018b076fe3
ol-biaa <olbiaaletta@gmail.com>
2024-05-13 12:52:17 -0700

    Row

--- netflix-clone/src/Pages/Home/Home.js
+++ netflix-clone/src/Pages/Home/Home.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
 import Banner from '../../Components/Banner/Banner'
+import RowList from '../../Components/Rows/RowLists/RowList'
 const Home = () => {
   return (
     <div>
      <Header /> 
      <Banner />
-     {/* <br /><br /><br /><br /><br /><br /><br /><br /> */}
+    <RowList />
      <Footer />
      
     </div>
--- netflix-clone/src/utils/requests.js
+++ netflix-clone/src/utils/requests.js
@@ -1,7 +1,7 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 const requests = {
-    fetchTreding:`/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRatedMovies:`/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
